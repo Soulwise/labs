46a5eca9bf10af0d4eaf76749fcabcf47855bd7c
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-02-01 09:28:44 +0200

    Initial commit

---
+++ python/examples/roulette.py
@@ -0,0 +1,14 @@
+import random
+
+
+def get_random(iterations=100, values=2):
+    result = [random.randint(0, 100) for _ in range(iterations)]
+    random.shuffle(result)
+    return [result[random.randint(0, iterations)] for _ in range(values)]
+
+
+def roulette():
+    values = get_random(iterations=200, values=6)
+    if values[0] > max(values[1:]):
+        return False
+    return True
