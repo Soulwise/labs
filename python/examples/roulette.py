76696729a8dc0ef3603291e85d860113acfae0f0
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-02-01 09:48:10 +0200

    Update default values

--- python/examples/roulette.py
+++ python/examples/roulette.py
@@ -3,14 +3,15 @@ from __future__ import print_function
 import random
 
 
-def get_random(iterations=100, values=2):
-    result = [random.randint(0, 100) for _ in range(iterations)]
+def get_random(iterations=200, values=6):
+    result = [random.randint(0, iterations * values)
+              for _ in range(iterations)]
     random.shuffle(result)
     return [result[random.randint(0, iterations)] for _ in range(values)]
 
 
 def roulette():
-    values = get_random(iterations=200, values=6)
+    values = get_random()
     if values[0] > max(values[1:]):
         return False
     return True
